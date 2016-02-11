e4d50b873d13cf65d12481978e2142442f1344ca
alyabihova <sunny4ka@gmail.com>
2016-02-11 22:47:10 +0200

    adds stub to validator code

--- node/server.js
+++ node/server.js
@@ -1,10 +1,8 @@
 var express = require('express');
-var fs = require('fs');
-var path = require('path');
 var bodyParser = require('body-parser');
 var log = require('./moduls/logger')(module);
 
-var queue = require('./moduls/queue');
+//var queue = require('./moduls/queue');
 var validate = require('./moduls/validator');
 
 var app = express();
@@ -23,34 +21,31 @@ var msg = {
 app.post('/isolatedTest', function (req, res) {
     // /isolatedTest?key=securityCode
     var securityCode = req.query.key;
-    validateKey(securityCode);
-
+    //validateKey(securityCode);
     var lang = req.body.language;
     var code = req.body.code;
     var testCases = req.body.testCases;
     if (lang && code && testCases) {
-        var dataInspection = validate({code: code, language: lang});
-        if (!dataInspection.validity) {
-            sendResponse(res, 200, 422, dataInspection.log)
-        }
+         var dataInspection = validate({code: code, language: lang});
+         if (!dataInspection.validity) {
+             sendResponse(res, 200, 422, dataInspection.log)
+         }
     }
     else
         sendErrorRes(res, '400');
     var id = new Date().getTime().toString();
-    queue.enqueue({id: {code: code, language: leng, testCases: testCases}}, function (err, data) {
+   /* queue.enqueue({sessionId: id, code: code, language: leng, testCases: testCases}, function (err, data) {
         if (err)
             throw err;
         sendResponse(res, 200, 200, data);
-    });
+    });*/
 
 
 });
 
-
 function sendResponse (res, statusCode, code, data) {
-    res.status(statusCode);
-    res.json({code : code, response: data});
-    res.end;
+    res.status(statusCode).json({code : code, response: data});
+    res.end();
 }
 
 
@@ -60,10 +55,9 @@ function validateKey(key) {
 
 function sendErrorRes(res, code) {
     log.error(msg[code]);
-    res.status(code);
-    res.send({'error': {'code': code, "message": msg[code]}});
+    res.status(code).json({'error': {'code': code, "message": msg[code]}});
+    res.end();
 }
-
 app.use(function (err, req, res) {
     sendErrorRes(res, 500);
 });
