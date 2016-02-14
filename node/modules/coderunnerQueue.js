d36a4e661729dacbcac35cbe2a4b45a4e971022e
vladimir <vprihogenko@gmail.com>
2016-02-14 20:57:53 +0200

    merge commit

--- node/modules/coderunnerQueue.js
+++ node/modules/coderunnerQueue.js
@@ -3,64 +3,53 @@
  */
 module.exports = RunnerQueue;
 
-function RunnerQueue() {
+function RunnerQueue () {
     this.arrPendingTasks = [];
     this.arrWorkingTasks = [];
     this.workingTasksCounter = 0;
 
+    // remove this when all tests are finished
     //var DockerRunner = require('./testExec3');
-    var DockerRunner = require('./DockerRunner');
-    this.dockerRunner = new DockerRunner();
+    var DockerRunner = require ('./DockerRunner');
+    this.dockerRunner = new DockerRunner ();
 
-    var config = require('../config.json');
+    var config = require ('../config.json');
     this.maxWorkingTaskNumber = config.MaxWorkingTaskNumber;
 }
 
 RunnerQueue.prototype.push = function (taskObj, callbackFunction) {
     if (this.workingTasksCounter < this.maxWorkingTaskNumber) {
-        this.sendTaskToDockerRunner(taskObj, callbackFunction );
+        this.sendTaskToDockerRunner (taskObj, callbackFunction);
     } else {
-        this.arrPendingTasks.push({ task: taskObj, cb: callbackFunction })
-        console.log("Task added to pending list",taskObj);
+        this.arrPendingTasks.push ({task: taskObj, cb: callbackFunction})
+        console.log ("Task added to pending list", taskObj);
     }
 };
 
 RunnerQueue.prototype.sendTaskToDockerRunner = function (taskObj, callbackFunction) {
-    var self=this;
+    var self = this;
+    var taskPos=this.arrWorkingTasks.length;
 
     var returnFunc = function (sessionId, answerObj) {
-        function findIndex(sId, arrWorkingTasks) {
-            var res=-1;
-            for(var i=0;i<arrWorkingTasks.length;i++){
-                if(arrWorkingTasks[i].task.sessionId==sId){
-                    res=i;
-                    break;
-                }
-            }
-            return res;
-        }
-
-        console.log("Task solution received from docker-manager",sessionId, answerObj);
-
-        var indexOfTask=findIndex(sessionId,self.arrWorkingTasks);
-        var callbackFunc=self.arrWorkingTasks[indexOfTask].cb;
-        self.arrWorkingTasks.splice(indexOfTask,1);
+        console.log ("Task solution received from docker-manager", sessionId, answerObj);
+        var callbackFunc = self.arrWorkingTasks[taskPos].cb;
+        self.arrWorkingTasks.splice (taskPos, 1);
         self.workingTasksCounter--;
 
-        if ((self.workingTasksCounter < self.maxWorkingTaskNumber) && (self.arrPendingTasks.length>0)) {
-            var taskToSolve=self.arrPendingTasks.shift();
-            self.sendTaskToDockerRunner(taskToSolve.task, taskToSolve.cb);
+        if ((self.workingTasksCounter < self.maxWorkingTaskNumber) && (self.arrPendingTasks.length > 0)) {
+            var taskToSolve = self.arrPendingTasks.shift ();
+            self.sendTaskToDockerRunner (taskToSolve.task, taskToSolve.cb);
         }
-
-        callbackFunc(answerObj);
+        callbackFunc (answerObj);
     };
 
-    this.dockerRunner.run(taskObj, returnFunc);
-    this.arrWorkingTasks.push({ task: taskObj, cb: callbackFunction} );
+    this.arrWorkingTasks.push ({task: taskObj, cb: callbackFunction});
+    this.dockerRunner.run (taskObj, returnFunc);
     this.workingTasksCounter++;
-    console.log("Task sent to docker-manager",taskObj);
+    console.log ("Task sent to docker-manager", taskObj);
 };
 
+// remove this when all tests are finished
 //var dq=new RunnerQueue();
 //dq.push({sessionId: "111"}, alldone);
 //dq.push({sessionId: "222"}, alldone);
