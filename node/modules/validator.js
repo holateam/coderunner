8ec93b433bb7eb693e129697467fbc164527a367
alyabihova <sunny4ka@gmail.com>
2016-02-15 14:05:11 +0200

    corrects regExp

--- node/modules/validator.js
+++ node/modules/validator.js
@@ -9,17 +9,17 @@ function cppValidator(soursecode) {
     soursecode.code = soursecode.code.replace(/asm/g,'aaa'); //rename all 'asm' to avoid assembler inserts
 
     var pos, end_pos; // start and finish position of searching string
-    var regPattern = /\u0023(.+)/;  //pattern for string with any number of any symbols enclosed between '#' and '\n'
-    var end = /\n/; //end of pattern string that starts with '#'
+    var regPattern = /(\n|^)\s*\u0023(.+)(\n|$)/;  //pattern for string that starts with any number of whitespace and contains any number of any symbols enclosed between '#' and '\n'
+    var end = /(\n|$)/; //end of pattern string that starts with '#'
     var code = soursecode.code;
     while ((pos = code.search(regPattern)) != -1) {
         code = code.substr(pos);
         end_pos = code.search(end);
         var pattern = code.substr(0, end_pos);
-        if (!(pattern.replace(/\s/g,'') in config.includes.acceptedCpp)) {
-            log.push({"danger-level": 2, "text": pattern, "comment": "Not allowed to use"});
+        if ((pattern.trim()) && !(pattern.replace(/\s/g,'') in config.includes.acceptedCpp)) {
+            log.push({"danger-level": 2, "text": pattern.trim(), "comment": "Not allowed to use"});
         }
-        code = code.substr(end_pos+1);
+        code = code.substr(end_pos + 1);
     }
 
     if (log.length > 0)
