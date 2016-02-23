df42e3e27ae268c4afe7fd88f235ac022829f747
Karponter <karponter.tarino@gmail.com>
2016-02-23 16:51:51 +0200

    IDEA formated

--- node/modules/dockerRunner.js
+++ node/modules/dockerRunner.js
@@ -187,10 +187,9 @@ DockerRunner.prototype.mergeResponse = function (response) {
             this.response[property] = response[property];
         }
     }
-}
+};
 
 DockerRunner.prototype.deleteFolderRecursive = function (path) {
-	return;
     var _this = this;
     if (fs.existsSync(path)) {
         fs.readdirSync(path).forEach(function (file) {
