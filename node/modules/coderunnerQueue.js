14c879df738a90f2bcd45ac90a15c31ee08b9127
alyabihova <sunny4ka@gmail.com>
2016-02-25 12:39:21 +0200

    changes global coderunner to individual

--- node/modules/coderunnerQueue.js
+++ node/modules/coderunnerQueue.js
@@ -10,8 +10,6 @@ function RunnerQueue () {
 
     // remove this when all tests are finished
     //var DockerRunner = require('./testExec3');
-    var DockerRunner = require ('./dockerRunner');
-    this.dockerRunner = new DockerRunner ();
 
     var config = require ('../config.json');
     this.maxWorkingTaskNumber = config.MaxWorkingTaskNumber;
@@ -43,7 +41,9 @@ RunnerQueue.prototype.sendTaskToDockerRunner = function (taskObj, callbackFuncti
     };
 
     console.log ("Task sent to docker-manager", taskObj);
-    this.dockerRunner.run (taskObj, returnFunc);
+    var DockerRunner = require ('./dockerRunner');
+    var dockerRunner = new DockerRunner ();
+    dockerRunner.run (taskObj, returnFunc);
     this.workingTasksCounter++;
 };
 
