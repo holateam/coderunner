ed7b9ef5832f075ceaa9156987faa1d7d8b6c0dd
alyabihova <sunny4ka@gmail.com>
2016-02-29 12:47:22 +0200

    adds logging on server http://nonscire.pp.ua/request-logger/logme.php

--- node/server.js
+++ node/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var request = require('request');
 var log = require('./modules/logger');
 var env = require('node-env-file');
 var validateCode = require('./modules/codeValidator');
@@ -9,6 +10,8 @@ var checkUserConfig = require('./modules/configCorrector.js');
 var config = require('./config.json');
 var Queue = require('./modules/coderunnerQueue');
 var queue = new Queue();
+var uri = "http://nonscire.pp.ua/request-logger/logme.php";
+
 
 
 // read configs to process.env
@@ -36,31 +39,60 @@ var server = app.listen(process.env.SERVER_PORT, function () {
     log.info('Running on http://localhost:' + process.env.SERVER_PORT);
 });
 
-function sendResponse (res, statusCode, code, data) {
+
+
+function sendResponse (id, res, statusCode, code, data) {
+    saveOnServer({"sessionID": id, "response": {"code": code, "response": data}});
     res.status(statusCode).json({code: code, response: data});
     res.end();
 }
 
-function sendErrorResponse (res, code, message) {
-    log.error(message || getMessageByHTTPCode(code));
-
-    res.status(code).json({'error': {'code': code, "message": message || getMessageByHTTPCode(code)}});
+function sendErrorResponse (id, res, code, message) {
+    message = message || getMessageByHTTPCode(code);
+    log.error(message);
+    saveOnServer({"sessionID": id, "response": {"error": {"code": code, "message": message}}});
+    res.status(code).json({error: {code: code, message: message}});
     res.end();
+}
+
+function saveOnServer (data) {
+    request(
+        {
+            method: 'POST',
+            uri: uri,
+            json: {"secret" : "someKey", "data": data}
+        },
+        function (err, response){
+            if (err) {
+                log.info("ERROR: " + err + ", could not send request: ", data);
+            } else if (response.code == 200) {
+                console.log("request was sent successfully");
+            } else {
+                log.info("WARNING: code: " + response.code + ", could not send request: ", data;
+            }
+        });
 }
 
 function isolatedTestRoute (req, res) {
 
+    //var id = new Date().getTime().toString();
+    var id = "" + Math.random();
+    id = id.substr(2);
+    
     log.info("********************************************************************************************");
-    log.info('Incoming request. Lng: ' + req.body.language + ", num testcases: " + req.body.testCases.length + ", code: " + req.body.code.substr(0, 50));
+    log.info('Incoming request. Session ID:' + id + ' Lng: ' + req.body.language + ", num testcases: " + req.body.testCases.length + ", code: " + req.body.code.substr(0, 50));
+
+    saveOnServer({"sessionID": id, "request": req.body});
+
 
     var userName = req.body.userName;
     var securityCode = req.body.serverSecret;
     if (userName && securityCode) {
         if (!validateKey(securityCode)) {
-            return sendErrorResponse(res, '403', 'Access denied');
+            return sendErrorResponse(id, res, '403', 'Access denied');
         }
     } else {
-        return sendErrorResponse(res, '400', 'Wrong parameters');
+        return sendErrorResponse(id, res, '400', 'Wrong parameters');
     }
 
     var lang = req.body.language;
@@ -72,19 +104,16 @@ function isolatedTestRoute (req, res) {
             dataInspection = validateTestCases(testCases);
         }
         if (!dataInspection.validity) {
-            return sendResponse(res, 200, 422, dataInspection.log);
+            return sendResponse(id, res, 200, 422, dataInspection.log);
         }
     } else {
-        return sendErrorResponse(res, '400', 'Wrong parameters');
+        return sendErrorResponse(id, res, '400', 'Wrong parameters');
     }
 
     var optionalConfig = req.body.optionalConfig;
     if (optionalConfig) {
         checkUserConfig(optionalConfig);
     }
-
-    //var id = new Date().getTime().toString();
-    var id=""+Math.random(); id=id.substr(2);
 
     log.info("Pushing request " + id + " to the CoderunnerQueue");
 
@@ -95,10 +124,10 @@ function isolatedTestRoute (req, res) {
         data.codeRunnerVersion = config.version;
 
         if (err) {
-            sendErrorResponse(res, 500, 'Internal server error');
+            sendErrorResponse(id, res, 500, 'Internal server error');
         } else {
             log.info("Sending answer to " + id + ": ", data);
-            sendResponse(res, 200, 200, data);
+            sendResponse(id, res, 200, 200, data);
         }
     });
 }
